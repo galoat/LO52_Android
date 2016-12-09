fe7822035ba9be2130eabd17db3c93efb3d5cdd8
Mathieu Fanchin <Mathieu Fanchin>
2016-12-09 11:42:38 +0100

    first file explorer

--- Mandroid/app/src/main/java/com/example/mathieu/mandroid/MainActivity.java
+++ Mandroid/app/src/main/java/com/example/mathieu/mandroid/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.mathieu.mandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        Intent i = new Intent(this, AndroidExplorer.class);
+        startActivity(i);
     }
 }
