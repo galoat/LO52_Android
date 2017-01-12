c312ee334390c40601d5add5a3170e8ef52ef74c
galoat <florianlacour@hotmail.fr>
2017-01-12 22:30:18 +0100

    meilleur implementation du drawer listenner

--- Mandroid/app/src/main/java/com/example/mathieu/mandroid/Adapters/FileArrayMetadataAdapter.java
+++ Mandroid/app/src/main/java/com/example/mathieu/mandroid/Adapters/FileArrayMetadataAdapter.java
@@ -44,6 +44,12 @@ public class FileArrayMetadataAdapter extends ArrayAdapter<ItemMetadata> {
         return items.get(i);
     }
 
+    public void removeBindDrawerListenner(){
+        if(drawerListener!=null){
+            drawerLayout.removeDrawerListener(drawerListener);
+            drawerListener=null;
+        }
+    }
     @NonNull
     @Override
     public View getView(int position, final View convertView, final ViewGroup parent) {
@@ -68,10 +74,7 @@ public class FileArrayMetadataAdapter extends ArrayAdapter<ItemMetadata> {
             }
 
          }
-        if(drawerListener!=null){
-            drawerLayout.removeDrawerListener(drawerListener);
-            drawerListener=null;
-        }
+
 
         Button buttonPlay = (Button) v.findViewById(R.id.PlayButton);
 
@@ -81,7 +84,7 @@ public class FileArrayMetadataAdapter extends ArrayAdapter<ItemMetadata> {
 
                 drawerLayout = (DrawerLayout) ((Activity) getContext()).getWindow().getDecorView().findViewById(R.id.drawer_layout);
                 drawerLayout.closeDrawers();
-                drawerListener = new DrawerEndListenner(o.getPath(), parent, drawerLayout);
+                drawerListener = new DrawerEndListenner(o.getPath(), parent,FileArrayMetadataAdapter.this);
                 drawerLayout.addDrawerListener(drawerListener);
             }
         });
